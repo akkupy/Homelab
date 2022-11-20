9c2dab719cbe279395f8ac6025a14e18d5ef8177
akkupy <akkupy@proton.me>
2022-11-20 13:01:02 +0530

    Added nextcloud doc

---
+++ docs/nextcloud.md
@@ -0,0 +1,19 @@
+# Nextcloud
+
+![](../images/nextcloud.png)
+
+## Introduction
+
+[Nextcloud](https://nextcloud.com/),A safe home for all your data. Access & share your files, calendars, contacts, mail & more from any device, on your terms.
+
+# Installation
+
+## Method 1 (Auto):
+
+1. **Run the following script**
+
+```
+wget -qO- https://raw.githubusercontent.com/akkupy/Homelab/main/scripts/install_nextcloud.sh | sudo bash
+```
+
+This will preserve any persistent data under /nextcloud and /mysql of User Directory, you can adapt the path to whatever suits you.
---
+++ scripts/install_nextcloud.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+
+echo "Creating directories..."
+sudo mkdir -p /home/$USER/nextcloud || error "Failed to create nextcloud folder!"
+sudo mkdir -p /home/$USER/mysql || error "Failed to create mysql folder!"
+
+echo "Downloading nextcloud container."
+docker pull nextcloud
+echo "Downloading mysql container."
+docker pull mysql
+echo "Initialising mysql"
+docker run --name mysql \
+    -e MYSQL_ROOT_PASSWORD=password \
+    -e MYSQL_DATABASE=nextcloud \
+    -e MYSQL_USER=nextcloud \
+    -e MYSQL_PASSWORD=nextcloud \
+    -v /home/$USER/mysql:/var/lib/mysql
+    --restart unless-stopped \
+    -d mysql
+echo "Initialising nextcloud"
+docker run --name nextcloud \
+    -d -p 7000:80 \
+    -v /home/$USER/nextcloud:/var/www/html \
+    --restart unless-stopped \
+    nextcloud
+echo " Done!"
